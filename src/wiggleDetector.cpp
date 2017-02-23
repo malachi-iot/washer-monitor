a02b4724c2e92c55922db7403c0fcf96665d98d2
Malachi Burke <malachi.burke@gmail.com>
2017-02-23 15:06:38 -0800

    Utilizing new layer1 more space/code efficient CircularBuffer
    NOTE: some oddness with memory alignment, kludging fix for now

--- src/wiggleDetector.cpp
+++ src/wiggleDetector.cpp
@@ -11,10 +11,8 @@ extern const int SENSOR_PIN;
 
 volatile uint16_t wigglesDetected = 0;
 
-uint16_t wigglesPerSecondBuf[60]; // for last 60 seconds
-util::CircularBuffer<uint16_t> wigglesPerSecond(wigglesPerSecondBuf, 60);
-uint32_t wigglesPerMinuteBuf[60];
-util::CircularBuffer<uint32_t> wigglesPerMinute(wigglesPerMinuteBuf, 60);
+util::layer1::CircularBuffer<uint16_t, 60> wigglesPerSecond;
+util::layer1::CircularBuffer<uint32_t, 60> wigglesPerMinute;
 
 void wiggleDetector()
 {
@@ -22,26 +20,34 @@ void wiggleDetector()
 }
 
 
-uint8_t minutesCounter = 0;
+// FIX: Some kind of odd alignment issue if I use a uint8_t here, so kludging it
+// and using a uint32_t .  I expect is related to above CircularBuffers perhaps not landing
+// on perfect alignments
+//__attribute__((section(".iram0.text"))) uint8_t minutesCounter = 0;
+uint32_t minutesCounter = 0;
 
 uint32_t getTotalWigglesInLast60Seconds();
 
 void printWiggleStatus()
 {
     uint16_t wd = wigglesDetected;
-    wigglesPerSecond.put(wd);
-    uint32_t wd60s = getTotalWigglesInLast60Seconds();
-
-    Serial.print("Wiggles detected/second: ");
-    Serial.print(wd);
-    Serial.print(" of ");
-    Serial.print(wd60s);
-    Serial.println(" in the last 60 seconds");
 
     if(wd > 0)
     {
         wigglesDetected = 0;
     }
+
+    wigglesPerSecond.put(wd);
+
+    uint32_t wd60s = getTotalWigglesInLast60Seconds();
+
+    Serial.print("Wiggles detected/second (");
+    Serial.print(minutesCounter);
+    Serial.print("): ");
+    Serial.print(wd);
+    Serial.print(" of ");
+    Serial.print(wd60s);
+    Serial.println(" in the last 60 seconds");
 
     if(++minutesCounter == 60)
     {
@@ -52,13 +58,16 @@ void printWiggleStatus()
     }
 }
 
+
 uint32_t getTotalWigglesInLast60Seconds()
 {
     uint32_t total = 0;
 
+    auto array = wigglesPerSecond.getUnderlyingArray();
+
     for(int i = 0; i < 60; i++)
     {
-        total += wigglesPerSecondBuf[i];
+        total += array[i];
     }
 
     return total;
